7d0bbfc9545fc336ae01cdb62e2e845e53f023a9
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-02 10:13:01 +0600

    env setup done

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-//user pass
-// car-service
-//cdtAWu1kRiJHV9dM
 
-user =process.env.DB_USER
-pass = process.env.DB_PASS
-const uri = "mongodb+srv://<username>:<password>@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+console.log(uri)
 
 app.get('/',(req,res)=>{
     res.send('car servicing server is running')
