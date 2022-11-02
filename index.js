7ceae114b79d0a9ea907d4569308ceb48f26825a
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-02 10:07:55 +0600

    environment variable setup for username and password

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require ('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require ('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+//middleware 
 app.use(cors())
 app.use(express.json())
+
+//user pass
+// car-service
+//cdtAWu1kRiJHV9dM
+
+user =process.env.DB_USER
+pass = process.env.DB_PASS
+const uri = "mongodb+srv://<username>:<password>@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.get('/',(req,res)=>{
     res.send('car servicing server is running')
 })
