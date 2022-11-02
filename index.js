480a98ea8ea5e0d442e8c550db2f7303d0ea5eb2
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-02 10:16:33 +0600

    some changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 //middleware 
 app.use(cors())
 app.use(express.json())
-
+//some changes on gi
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
