4cba37aebcf68ebac68c16ad007857aa5871e1f2
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-02 23:44:25 +0600

    order delete from database api calling done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require ('express')
+const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require ('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 //middleware 
@@ -11,25 +11,64 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //insert data to mongodb
-async function run(){
-    try{
-    const dataCollection = client.db('car-service').collection('car-service-users')
-    const user = {name: 'mahfuzur',email:'mfz@gmail.com'}
-    const result = await dataCollection.insertOne(user)
-    console.log(result)
+async function run() {
+    try {
+        const dataCollection = client.db('car-service').collection('car-service-users');
+        const ordersCollection = client.db('car-service').collection('orders')
+        //find multiple data from mongodb calling api
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = dataCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        //find a single data from mongo db api calling
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await dataCollection.findOne(query)
+            res.send(service)
+        })
+
+        //orders api : set data using post method from express
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order)
+            res.send(result)
+        })
+        //get orders data for selected logged in user;
+        app.get('/orders', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
+            }
+            const cursor = ordersCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        //delete order 
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.send(result)
+
+        })
     }
-    finally{
+
+    finally {
 
     }
 }
 
-run().catch(err => console.log(err))
+run().catch(err => console.error(err))
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('car servicing server is running')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`This port running on ${port}`)
 })
