be29178470ec86d0b5885a6209eb3cc7148ab627
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-02 10:43:04 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ const port = process.env.PORT || 5000;
 //middleware 
 app.use(cors())
 app.use(express.json())
-//some changes on gi
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
+//insert data to mongodb
+async function run(){
+    try{
+    const dataCollection = client.db('car-service').collection('car-service-users')
+    const user = {name: 'mahfuzur',email:'mfz@gmail.com'}
+    const result = await dataCollection.insertOne(user)
+    console.log(result)
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+
 
 app.get('/',(req,res)=>{
     res.send('car servicing server is running')
